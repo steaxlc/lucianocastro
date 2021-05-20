13e9514f1575fe0053c5b9f9ccd85d7a8d8decd5
Luciano Castro <contatolucianocastro@gmail.com>
2021-05-20 12:52:06 -0300

    correction bugs and new animation

--- components/Nav.js
+++ components/Nav.js
@@ -1,117 +1,35 @@
-import React, { useState } from 'react';
-import styled from 'styled-components'
-import { FaSun, FaMoon } from "react-icons/fa";
+import React, { useState } from 'react'
+//icons
+import {FaSun} from 'react-icons/fa'
+//css
+import styles from '../styles/nav.module.css'
 
-const Nav = ({darkMode,setDarkMode}) => {
- 
-    const darkModeHandler = () => {
-        setDarkMode(!darkMode);
-        localStorage.setItem('darkMode', darkMode);
-    }
-
-    let menuOpen = false;
-    const menuBtn = document.querySelector('.menu-btn');
+const Nav = ({darkMode, setDarkMode}) => {
+    const [menuOpen, setMenuOpen] = useState(false);
     const hamburguerAnimationHandler = () => {
-        
-        if (menuOpen) {
-            menuBtn.classList.remove('open');
-            menuOpen = false;
-        } else { 
-            menuBtn.classList.add('open');
-            menuOpen = true;
-        }
+        setMenuOpen(!menuOpen);
+    }
+    const darkModeHandler = () => {
+        setDarkMode(!darkMode)
     }
 
     return (
-        <StyledNav darkMode={darkMode} >
-            <Logo>Luciano Castro</Logo>
-            <div className="clickable">
-                <div className="sunmoon"  onClick={darkModeHandler}>
-                {darkMode ? <FaSun style={{ fontSize: 30 }}/> : <FaMoon style={{ fontSize: 30 }}/>}
+        <div className={styles.stylednav}>
+            <div className={styles.logo}>
+                Luciano Castro
+            </div>
+            <div className={styles.clickable}>
+                <div className={styles.sunmoon} onClick={darkModeHandler}>
+                    <FaSun style={{fontSize: 25}}/>
                 </div>
-            <div className="menu-btn" onClick={hamburguerAnimationHandler}>
-                <div className="menu-btn-burger">
+                <div className={styles.menubtn + ` `+ `${menuOpen? styles.open : ``}`} onClick={hamburguerAnimationHandler}>
+                    <div className={styles.menubtnburguer}></div>
                 </div>
             </div>
-            </div>
-        </StyledNav>
+            
+        </div>
     )
 }
 
-
-const Logo = styled.a`
-    text-decoration: none;
-    cursor: pointer;
-    font-size: 1.8rem;
-    z-index: 1;
-    font-weight: bold;
-    
-`;
- 
-const StyledNav = styled.nav`
-    transition: all 0.5s ease-in-out;
-    z-index: 5; 
-    align-items: center;
-    margin: 15px 30px 15px 30px;
-    display: flex;
-    justify-content: space-between;
-    .sunmoon{
-        margin-top: 7px;
-        cursor: pointer;
-    }
-    .clickable{
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        
-    }
-    .menu-btn.open .menu-btn-burger{
-        transform: translateX(-40px);
-        background: transparent;
-        ::before{
-            transform: rotate(45deg) translate(35px, -35px);
-        }
-        ::after{
-            transform: rotate(-45deg) translate(35px, 35px);
-        }
-    }
-    .menu-btn{
-        position: relative;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 50px;
-    height: 50px;
-    cursor: pointer;
-    margin-left: 15px;
-    transition: all .5s ease-in-out;
-    .menu-btn-burger{
-        width: 40px;
-        height: 4px;
-        ${props => props.darkMode ? `
-        background: white;
-        `: `background: black;`}
-        border-radius: 5px;
-        transition: all .5s ease-in-out;
-        ::after, ::before {
-            content: '';
-            position: absolute;
-            width: 40px;
-            height: 4px;
-            ${props => props.darkMode ? `
-            background: white;
-            `: `background: black;`}
-            border-radius: 5px;
-            transition: all .5s ease-in-out;
-        }
-        ::before{
-            transform: translateY(-12px);
-        }
-        ::after{
-            transform: translateY(12px);
-        }
-    }
-    }
-`;
 
 export default Nav
--- pages/index.js
+++ pages/index.js
@@ -1,74 +1,38 @@
 import Head from 'next/head'
-import styled from 'styled-components'
 import Nav from '../components/Nav'
-import React, { useState, useEffect } from 'react';
+import styles from '../styles/home.module.css'
+
+import {useState} from 'react'
 
 export default function Home() {
-  let getDarkMode = localStorage.getItem('darkMode')
-    if (!getDarkMode)
-        getDarkMode = true;
-  const [darkMode, setDarkMode] = useState(getDarkMode);
 
-  let classDark = 'dark '
-  useEffect(() => {
-    if (darkMode) {
-      classDark += 'open'
-    } else {
-      classDark ='dark '
-    }
-    console.log(classDark);
-  }, [darkMode])
+  const [darkMode, setDarkMode] = useState(true);
+
   return ( 
-    <StyledHome darkMode={darkMode}>
+    <div className={styles.main + ` `+ `${darkMode? styles.darktheme : ``}`}>
       <Head>
         <title>Luciano Castro - Portfolio</title>
         <meta name="description" content="Luciano Castro - Portfolio" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Nav darkMode={darkMode} setDarkMode={ setDarkMode}/>
-      <p>
-        Luciano Castro
-      </p>    
-      <div className= 'dark ' >
+      <Nav
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
+      <div className={styles.teste}>
+        teste
+      </div>
+      <div className={styles.teste}>
+        teste
+      </div>
+      <div className={styles.teste}>
+        teste
+      </div>
+      <div className={styles.teste}>
+        teste
       </div>
       
-    </StyledHome>
+    </div>
   )
-} 
+}
 
-const StyledHome = styled.div`
-  position: fixed;
-  transition: all 0.5s ease-in-out;
-  top:0;
-  left: 0;
-  width: 100%; 
-  min-height: 100vh;
-  background: linear-gradient( to top, #020c17, #072142);
-  font-family: 'Poppins', sans-serif;
-  ${props => props.darkMode ? `
-    
-    color: white;
-  `: `
-  color: black;`}
-  
-  .dark{
-    position: fixed;
-    top: 35px;
-    right: 107px;
-    z-index: -1;
-    border-radius: 50%;
-    background-image: linear-gradient( to top, #4178dd, #b2defe);
-    transition: all 1s ease-in-out;
-    ${props => props.darkMode ? `
-    width: 0px;
-    height: 0px;
-  `: `
-    top: 0;
-    right: 0;
-    width: inherit;
-    height: 100vh;
-    z-index: -1;
-    border-radius: 0%;
-  `}
-  }
-  `;
